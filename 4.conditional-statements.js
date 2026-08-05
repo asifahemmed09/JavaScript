986e5e6fc0569068f95de7822f361522b0e2986e
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-08-05 17:57:22 +0600

    conditional statements

---
+++ 4.conditional-statements.js
@@ -0,0 +1,28 @@
+// if statement
+let age = 25
+if(age >= 18){
+  console.log("You are an adult")
+}
+
+// if-else statement
+let balance = 1000
+let withdrawAmount = 700
+
+if(balance >= withdrawAmount){
+  console.log("Transaction Successful")
+}
+else{
+  console.log("Insufficient Balance")
+}
+
+
+// if-else-if statement
+if(age <= 12){
+  console.log("You are a minor")
+}
+else if(age > 12 && age <= 65){
+  console.log("You are an adult")
+}
+else{
+  console.log("You are a senior citizen")
+}
