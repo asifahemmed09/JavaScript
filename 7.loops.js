6426ee73d7d154be8955d0906291c7656cf692b6
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-08-05 18:38:37 +0600

    loops

---
+++ 7.loops.js
@@ -0,0 +1,21 @@
+// for loop
+let i;
+
+for(i = 1; i <= 5; i++){
+  console.log(i)
+}
+
+// while loop
+i = 1
+while(i <= 5){
+  console.log("*")
+  i++
+}
+
+// Do-While Loop
+i = 0
+do{
+  console.log(i)
+  i++
+}while(i < 10)
+
